61c5cbd12febd696d5aea65e18af2f8500440e40
Sean Thomas <Sean.M.Thomas.0@gmail.com>
2016-10-12 12:49:27 -0400

    add level 1 solution

---
+++ Level 1/i_love_lance_janice.java
@@ -0,0 +1,48 @@
+//Welcome to foobar version 53-10-g85713ac-beta (2016-09-28-19:53+0000)
+public class i_love_lance_janice {
+    /*
+        I Love Lance & Janice
+        =====================
+
+        You've caught two of your fellow minions passing coded notes back and forth - while they're on duty, no less! Worse, you're pretty sure it's not job-related - they're both huge fans of the space soap opera "Lance & Janice". You know how much Commander Lambda hates waste, so if you can prove that these minions are wasting her time passing non-job-related notes, it'll put you that much closer to a promotion. 
+
+        Fortunately for you, the minions aren't exactly advanced cryptographers. In their code, every lowercase letter [a..z] is replaced with the corresponding one in [z..a], while every other character (including uppercase letters and punctuation) is left untouched.  That is, 'a' becomes 'z', 'b' becomes 'y', 'c' becomes 'x', etc.  For instance, the word "vmxibkgrlm", when decoded, would become "encryption".
+
+        Write a function called answer(s) which takes in a string and returns the deciphered string so you can show the commander proof that these minions are talking about "Lance & Janice" instead of doing their jobs.
+
+
+        Languages
+        =========
+
+        To provide a Python solution, edit solution.py
+        To provide a Java solution, edit solution.java
+
+        Test cases
+        ==========
+
+        Inputs:
+            (string) s = "wrw blf hvv ozhg mrtsg'h vkrhlwv?"
+        Output:
+            (string) "did you see last night's episode?"
+
+        Inputs:
+            (string) s = "Yvzs! I xzm'g yvorvev Lzmxv olhg srh qly zg gsv xlolmb!!"
+        Output:
+            (string) "Yeah! I can't believe Lance lost his job at the colony!!"
+
+        Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
+    */
+    public static String answer(String s) { 
+
+        // Your code goes here.
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if( c >= 'a' && c <= 'z' ) {
+                c = (char)((int)('z' - c + 'a')); 
+            }
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
